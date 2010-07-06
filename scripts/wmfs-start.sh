f6dc45b47f80ffd023250659a8418735f01681f4
Julien STECHELE <fl4t@hotmail.fr>
2010-07-06 16:05:56 +0200

    test

--- scripts/wmfs-start.sh
+++ scripts/wmfs-start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#(sleep 1s && exec ~/.conky/wmfs/conky_start_wmfs.sh) 
-(sleep 1s && xcompmgr -c -t-5 -l-5 -r4.2 -o.55) &
-#(sleep 1s && devilspie) &
+source $HOME/.config/wmfs/global.conf
+exec $CONKYCMD > $FICHIER 2> /dev/null &
+xcompmgr -CcFf -t-5 -l-5 -r4.2 -o.55 &
 wmfs
-#cairo-compmgr -n &
-#xcompmgr -c -C -r 2 -l 0 -t 0
